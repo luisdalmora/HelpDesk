95a7624118e2e6359fb99b1e96a4e5b187282309
Luis Henrique <132011041+luisdalmora@users.noreply.github.com>
2023-08-21 16:35:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,333 @@
+function _defineProperty(obj, key, value) {
+  if (key in obj) {
+    Object.defineProperty(obj, key, {
+      value: value,
+      enumerable: true,
+      configurable: true,
+      writable: true,
+    });
+  } else {
+    obj[key] = value;
+  }
+  return obj;
+}
+const ANIMATION_DURATION = 300;
+
+const SIDEBAR_EL = document.getElementById("sidebar");
+
+const SUB_MENU_ELS = document.querySelectorAll(
+  ".menu > ul > .menu-item.sub-menu"
+);
+
+const FIRST_SUB_MENUS_BTN = document.querySelectorAll(
+  ".menu > ul > .menu-item.sub-menu > a"
+);
+
+const INNER_SUB_MENUS_BTN = document.querySelectorAll(
+  ".menu > ul > .menu-item.sub-menu .menu-item.sub-menu > a"
+);
+
+class PopperObject {
+  constructor(reference, popperTarget) {
+    _defineProperty(this, "instance", null);
+    _defineProperty(this, "reference", null);
+    _defineProperty(this, "popperTarget", null);
+    this.init(reference, popperTarget);
+  }
+
+  init(reference, popperTarget) {
+    this.reference = reference;
+    this.popperTarget = popperTarget;
+    this.instance = Popper.createPopper(this.reference, this.popperTarget, {
+      placement: "right",
+      strategy: "fixed",
+      resize: true,
+      modifiers: [
+        {
+          name: "computeStyles",
+          options: {
+            adaptive: false,
+          },
+        },
+
+        {
+          name: "flip",
+          options: {
+            fallbackPlacements: ["left", "right"],
+          },
+        },
+      ],
+    });
+
+    document.addEventListener(
+      "click",
+      (e) => this.clicker(e, this.popperTarget, this.reference),
+      false
+    );
+
+    const ro = new ResizeObserver(() => {
+      this.instance.update();
+    });
+
+    ro.observe(this.popperTarget);
+    ro.observe(this.reference);
+  }
+
+  clicker(event, popperTarget, reference) {
+    if (
+      SIDEBAR_EL.classList.contains("collapsed") &&
+      !popperTarget.contains(event.target) &&
+      !reference.contains(event.target)
+    ) {
+      this.hide();
+    }
+  }
+
+  hide() {
+    this.instance.state.elements.popper.style.visibility = "hidden";
+  }
+}
+
+class Poppers {
+  constructor() {
+    _defineProperty(this, "subMenuPoppers", []);
+    this.init();
+  }
+
+  init() {
+    SUB_MENU_ELS.forEach((element) => {
+      this.subMenuPoppers.push(
+        new PopperObject(element, element.lastElementChild)
+      );
+
+      this.closePoppers();
+    });
+  }
+
+  togglePopper(target) {
+    if (window.getComputedStyle(target).visibility === "hidden")
+      target.style.visibility = "visible";
+    else target.style.visibility = "hidden";
+  }
+
+  updatePoppers() {
+    this.subMenuPoppers.forEach((element) => {
+      element.instance.state.elements.popper.style.display = "none";
+      element.instance.update();
+    });
+  }
+
+  closePoppers() {
+    this.subMenuPoppers.forEach((element) => {
+      element.hide();
+    });
+  }
+}
+
+const slideUp = (target, duration = ANIMATION_DURATION) => {
+  const { parentElement } = target;
+  parentElement.classList.remove("open");
+  target.style.transitionProperty = "height, margin, padding";
+  target.style.transitionDuration = `${duration}ms`;
+  target.style.boxSizing = "border-box";
+  target.style.height = `${target.offsetHeight}px`;
+  target.offsetHeight;
+  target.style.overflow = "hidden";
+  target.style.height = 0;
+  target.style.paddingTop = 0;
+  target.style.paddingBottom = 0;
+  target.style.marginTop = 0;
+  target.style.marginBottom = 0;
+  window.setTimeout(() => {
+    target.style.display = "none";
+    target.style.removeProperty("height");
+    target.style.removeProperty("padding-top");
+    target.style.removeProperty("padding-bottom");
+    target.style.removeProperty("margin-top");
+    target.style.removeProperty("margin-bottom");
+    target.style.removeProperty("overflow");
+    target.style.removeProperty("transition-duration");
+    target.style.removeProperty("transition-property");
+  }, duration);
+};
+const slideDown = (target, duration = ANIMATION_DURATION) => {
+  const { parentElement } = target;
+  parentElement.classList.add("open");
+  target.style.removeProperty("display");
+  let { display } = window.getComputedStyle(target);
+  if (display === "none") display = "block";
+  target.style.display = display;
+  const height = target.offsetHeight;
+  target.style.overflow = "hidden";
+  target.style.height = 0;
+  target.style.paddingTop = 0;
+  target.style.paddingBottom = 0;
+  target.style.marginTop = 0;
+  target.style.marginBottom = 0;
+  target.offsetHeight;
+  target.style.boxSizing = "border-box";
+  target.style.transitionProperty = "height, margin, padding";
+  target.style.transitionDuration = `${duration}ms`;
+  target.style.height = `${height}px`;
+  target.style.removeProperty("padding-top");
+  target.style.removeProperty("padding-bottom");
+  target.style.removeProperty("margin-top");
+  target.style.removeProperty("margin-bottom");
+  window.setTimeout(() => {
+    target.style.removeProperty("height");
+    target.style.removeProperty("overflow");
+    target.style.removeProperty("transition-duration");
+    target.style.removeProperty("transition-property");
+  }, duration);
+};
+
+const slideToggle = (target, duration = ANIMATION_DURATION) => {
+  if (window.getComputedStyle(target).display === "none")
+    return slideDown(target, duration);
+  return slideUp(target, duration);
+};
+
+const PoppersInstance = new Poppers();
+
+/**
+ * wait for the current animation to finish and update poppers position
+ */
+const updatePoppersTimeout = () => {
+  setTimeout(() => {
+    PoppersInstance.updatePoppers();
+  }, ANIMATION_DURATION);
+};
+
+/**
+ * sidebar collapse handler
+ */
+document.getElementById("btn-collapse").addEventListener("click", () => {
+  SIDEBAR_EL.classList.toggle("collapsed");
+  PoppersInstance.closePoppers();
+  if (SIDEBAR_EL.classList.contains("collapsed"))
+    FIRST_SUB_MENUS_BTN.forEach((element) => {
+      element.parentElement.classList.remove("open");
+    });
+
+  updatePoppersTimeout();
+});
+
+/**
+ * sidebar toggle handler (on break point )
+ */
+document.getElementById("btn-toggle").addEventListener("click", () => {
+  SIDEBAR_EL.classList.toggle("toggled");
+
+  updatePoppersTimeout();
+});
+
+/**
+ * toggle sidebar on overlay click
+ */
+document.getElementById("overlay").addEventListener("click", () => {
+  SIDEBAR_EL.classList.toggle("toggled");
+});
+
+const defaultOpenMenus = document.querySelectorAll(".menu-item.sub-menu.open");
+
+defaultOpenMenus.forEach((element) => {
+  element.lastElementChild.style.display = "block";
+});
+
+/**
+ * handle top level submenu click
+ */
+FIRST_SUB_MENUS_BTN.forEach((element) => {
+  element.addEventListener("click", () => {
+    if (SIDEBAR_EL.classList.contains("collapsed"))
+      PoppersInstance.togglePopper(element.nextElementSibling);
+    else {
+      const parentMenu = element.closest(".menu.open-current-submenu");
+      if (parentMenu)
+        parentMenu
+          .querySelectorAll(":scope > ul > .menu-item.sub-menu > a")
+          .forEach(
+            (el) =>
+              window.getComputedStyle(el.nextElementSibling).display !==
+                "none" && slideUp(el.nextElementSibling)
+          );
+
+      slideToggle(element.nextElementSibling);
+    }
+  });
+});
+
+/**
+ * handle inner submenu click
+ */
+INNER_SUB_MENUS_BTN.forEach((element) => {
+  element.addEventListener("click", () => {
+    slideToggle(element.nextElementSibling);
+  });
+});
+
+/*---------------Tema---------------*/
+const themeToggle = document.getElementById("btn-toggle");
+const body = document.body;
+
+// Função para alternar o tema
+function toggleTheme() {
+  body.classList.toggle("dark-theme");
+  body.classList.toggle("light-theme");
+}
+
+// Evento de clique no botão de alternar tema
+themeToggle.addEventListener("click", () => {
+  toggleTheme();
+  // Armazenar o tema atual no armazenamento local
+  if (body.classList.contains("dark-theme")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
+
+// Função para definir o tema com base no armazenamento local
+function setThemeFromLocalStorage() {
+  if (localStorage.getItem("theme") === "dark") {
+    body.classList.add("dark-theme");
+  } else {
+    body.classList.add("light-theme");
+  }
+}
+
+// Evento ao carregar a página para definir o tema com base no armazenamento local
+window.addEventListener("load", () => {
+  setThemeFromLocalStorage();
+});
+
+// Evento de clique nos itens de tema no menu
+const themeMenuItems = document.querySelectorAll(".menu-title");
+themeMenuItems.forEach((menuItem) => {
+  menuItem.addEventListener("click", () => {
+    const themeName = menuItem.textContent.toLowerCase();
+    if (themeName === "dark" || themeName === "light") {
+      if (
+        (themeName === "dark" && !body.classList.contains("dark-theme")) ||
+        (themeName === "light" && !body.classList.contains("light-theme"))
+      ) {
+        toggleTheme();
+        // Armazenar o tema atual no armazenamento local
+        localStorage.setItem("theme", themeName);
+      }
+    }
+  });
+});
+
+/*---------------Maps---------------*/
+const openMapsLink = document.getElementById("openMaps");
+
+openMapsLink.addEventListener("click", () => {
+  // URL para a localização que deseja mostrar no Google Maps
+  const locationURL =
+    "https://www.google.com/maps/place/Av.+Aracruz,+818+-+Centro,+Linhares+-+ES,+29900-240/@-19.3916181,-40.0694726,19z/data=!4m9!1m2!2m1!1zTG9jYWxpemHDp8Ojbyc!3m5!1s0xb625e49ce085e5:0x155a10af7a64598a!8m2!3d-19.3916181!4d-40.0685785!16s%2Fg%2F11h7kcjypk?entry=ttu";
+
+  // Abre a URL em uma nova aba
+  window.open(locationURL, "_blank");
+});
+/*---------------Maps---------------*/
